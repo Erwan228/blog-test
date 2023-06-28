11212f9d5919f620466c481bc1671d49028d045c
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-28 11:17:56 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    
+  setup() {
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1 },
+      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2 }
+    ])
+
+    return { posts }
   }
 }
 </script>
