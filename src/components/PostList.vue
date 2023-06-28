346fe237c4afff613113fd3f4d24046373088fd3
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-28 12:34:47 +0200

    beneath

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="post-list">
-
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post"/>
+        </div>
     </div>
 </template>
 
 <script>
+import SinglePost from './SinglePost.vue'
 export default {
-    props: ['posts']
+    props: ['posts'],
+    setup(props) {
+        console.log(props.posts)
+    }
 }
 </script>
 
