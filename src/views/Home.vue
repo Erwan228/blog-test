e69e6721cf0eb3ef912f9999e5e9cdbb96f33520
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-28 14:35:49 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts"/>
+    <PostList v-if="showPosts" :posts="posts"/>
+    <button @click="showPosts = !showPosts">toggle posts</button>
   </div>
 </template>
 
@@ -19,8 +20,12 @@ export default {
       { title: 'welcome to the blog', body: 'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum', id: 1 },
       { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2 }
     ])
+    const showPosts = ref(true)
 
-    return { posts }
+    return { 
+      posts,
+      showPosts
+    }
   }
 }
 </script>
