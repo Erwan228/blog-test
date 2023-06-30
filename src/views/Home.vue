0b0cec1db7fbd57a88363df1af681c61803a4bad
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-30 10:22:38 +0200

    e

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,6 @@
 
 <script>
 import PostList from '@/components/PostList.vue'
-import { ref } from 'vue'
 
 export default {
   name: 'Home',
@@ -21,24 +20,7 @@ export default {
     PostList
   },
   setup() {
-    const posts = ref([])
-    const error = ref(null)
-
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw Error('no data available')
-        }
-        posts.value = await data.json()
-      }
-      catch (err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
-
-    load()
+    
 
     const showPosts = ref(true)
 
